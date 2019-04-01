2072ed0bf8fc0536efc3ea49b540185f8a362191
rlmarcos <werh79@gmail.com>
2019-04-01 22:55:36 +0200

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev' ){
 	urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-	urlDB = 'mongodb+srv://rlmarcos:qzqfeXd6Wtauebt7@cluster0-dx9zw.mongodb.net/cafe';
+	urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
